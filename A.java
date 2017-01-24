0fedece2584d82fa021deca6b175a7c5bfbc67ee
VHellendoorn <vhellendoorn@live.nl>
2017-01-24 14:02:16 -0800

    Update to A.java and fix layout in B.java

--- A.java
+++ A.java
@@ -14,8 +14,28 @@ public class A {
 		readGraph(file);
 	}
 
-	private void readGraph(File file) {
-		// TODO
+	private void readGraph(File file) throws FileNotFoundException, IOException {
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (line.startsWith("c")) continue;
+			
+			String[] split = line.split("\\s+");
+			if (line.startsWith("p")) {
+				int vertexCount = Integer.parseInt(split[2]);
+				int edgeCount = Integer.parseInt(split[3]);
+				this.vertices = new int[vertexCount];
+				this.edges = new boolean[vertexCount][vertexCount];
+				this.edgeCount = edgeCount;
+			}
+			else {
+				int left = Integer.parseInt(split[1]);
+				int right = Integer.parseInt(split[2]);
+				this.edges[left][right] = true;
+				this.edges[right][left] = true;
+			}
+		}
+		br.close();
 	}
 	
 	public int[] getVertices() {
